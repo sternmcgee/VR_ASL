eeb4506e4fa5a7f6d2ac429824c37cce3aed4dbe
JC Hamilton <jhamilton248@tamu.edu>
2019-09-19 10:00:35 -0500

    added lesson controller and letter change script

---
+++ Assets/LetterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LetterController : MonoBehaviour
+{
+
+    public GameObject GameObjec;
+    public SimpleHelvetica script;
+    // Start is called before the first frame update
+    void Start()
+    {
+        script = GameObjec.GetComponent<SimpleHelvetica>();
+
+        StartCoroutine(ChangeTextAFewTimes());
+    }
+
+    private IEnumerator ChangeTextAFewTimes()
+    {
+        yield return new WaitForSeconds(5);
+        UpdateText('B');
+        yield return new WaitForSeconds(5);
+        UpdateText('C');
+        yield return new WaitForSeconds(5);
+        UpdateText('D');
+    }
+
+   public void UpdateText(char c)
+    {
+        script.Text = c.ToString();
+        script.GenerateText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
