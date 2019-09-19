a88f75a094ad923dc9a2d2f5dd7d299dbac225b0
JC Hamilton <jhamilton248@tamu.edu>
2019-09-19 11:08:29 -0500

    added controlable 2d image of hand

--- Assets/LetterController.cs
+++ Assets/LetterController.cs
@@ -5,12 +5,16 @@ using UnityEngine;
 public class LetterController : MonoBehaviour
 {
 
-    public GameObject GameObjec;
-    public SimpleHelvetica script;
+    public GameObject FloatingLetter;
+    public GameObject FloatingImage;
+    public SimpleHelvetica LetterScript;
+    public Renderer ImageRenderer;
+    public Material[] Images;
     // Start is called before the first frame update
     void Start()
     {
-        script = GameObjec.GetComponent<SimpleHelvetica>();
+        LetterScript = FloatingLetter.GetComponent<SimpleHelvetica>();
+        ImageRenderer = FloatingImage.GetComponent<Renderer>();
 
         StartCoroutine(ChangeTextAFewTimes());
     }
@@ -19,16 +23,24 @@ public class LetterController : MonoBehaviour
     {
         yield return new WaitForSeconds(5);
         UpdateText('B');
+        UpdateImage('B');
         yield return new WaitForSeconds(5);
         UpdateText('C');
+        UpdateImage('C');
         yield return new WaitForSeconds(5);
         UpdateText('D');
+        UpdateImage('D');
     }
 
    public void UpdateText(char c)
     {
-        script.Text = c.ToString();
-        script.GenerateText();
+        LetterScript.Text = c.ToString();
+        LetterScript.GenerateText();
+    }
+
+    public void UpdateImage(char c)
+    {
+        ImageRenderer.material = Images[c - 'A'];
     }
 
     // Update is called once per frame
