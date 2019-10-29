86eafdd2a8b44c883f9e41c0804d26a82eaa7edf
JC Hamilton <jhamilton248@tamu.edu>
2019-10-29 10:29:57 -0500

    added scripts for throwing hands game;

---
+++ Assets/Scripts/RingController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingController : MonoBehaviour
+{
+    public GameObject letterObject;
+    private char letter;
+    private SimpleHelvetica letterScript;
+
+    public void AssignLetter(char c)
+    {
+        letter = c;
+        letterScript = letterObject.GetComponent<SimpleHelvetica>();
+        letterScript.Text = c.ToString();
+        letterScript.GenerateText();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ThrowingHandsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowingHandsController : MonoBehaviour
+{
+    public GameObject spawnPoint;
+    public GameObject[] rings;
+    public char[] allLetters;
+    public int numSpawns;
+    private int score;
+
+    public GameObject popfx;
+    public Material[] images;
+    public GameObject cube;
+    
+
+    void Start()
+    {
+        if(allLetters.Length > 4)
+        {
+            Debug.Log("Cannot use more than 4 letters; array will be truncated");
+        }
+
+        for(int i = 0; i < 4 && i < allLetters.Length; i++)
+        {
+            rings[i].GetComponent<RingController>().AssignLetter(allLetters[i]);
+        }
+    }
+    
+    void Update()
+    {
+        
+    }
+}
