59aa7a9575f8e2db3e88f9efe738f3eba664bbe5
Jack Belcher <jbelcher_97@tamu.edu>
2019-11-04 15:34:30 -0600

    Minor recorder edits

--- Assets/Scripts/Recorder/GloveRecorder.cs
+++ Assets/Scripts/Recorder/GloveRecorder.cs
@@ -11,17 +11,20 @@ public class GloveRecorder : MonoBehaviour
 
     private HI5_Source handSource;
 
-    private bool recording = false;     // we may or may not need this; let's see how it reacts in Update()...
+    private StreamWriter writer;
+
+    private bool recording = false;     // we may or may not need this, let's see how it reacts in Update()...
 
     private string dataPath = "../Data/";
+
 
     // Set up file for recording
     private void initializeWriter(Hand hand, Gesture gesture)
     {
-        StreamWriter writer = new StreamWriter(dataPath + hand + "_" + gesture + "_" + DateTime.Now + ".csv");
+        writer = new StreamWriter(dataPath + hand + "_" + gesture + "_" + DateTime.Now + ".csv");
         string header = "";
 
-        for( int i = 0; i < Enum.GetNames(typeof(Bones)).Length; ++i )
+        for(int i = 0; i < Enum.GetNames(typeof(Bones)).Length; ++i)
         {
             string[] cord = {"x", "y", "z", "w"};
 
@@ -30,7 +33,7 @@ public class GloveRecorder : MonoBehaviour
                 if(i == 0) { header += (Bones)i + "_pos" + cord[j]; }
                 else { header += "," + (Bones)i + "_pos" + cord[j]; }
             }
-            foreach( string s in cord )
+            foreach(string s in cord)
             {
                 header += "," + "_quot" + (Bones)i + s;
             }
@@ -38,21 +41,22 @@ public class GloveRecorder : MonoBehaviour
         header += ",gesture";
         writer.WriteLine(header);
 
-        writeData(hand, gesture, writer);
+        writeData(hand, gesture);
     }
 
+
     // Write hand data to csv file
-    private void writeData(Hand hand, Gesture gesture, StreamWriter writer)
+    private void writeData(Hand hand, Gesture gesture)
     {
         recording = true;
         string data = "";
 
-        for( int i = 0; i < Enum.GetNames(typeof(Bones)).Length; ++i )
+        for(int i = 0; i < Enum.GetNames(typeof(Bones)).Length; ++i)
         {
             Vector3 bonePos = handSource.GetReceivedPosition(i, hand);
             Vector3 boneRot = handSource.GetReceivedRotation(i, hand);
 
-            for( int j = 0; j < 3; ++j )
+            for(int j = 0; j < 3; ++j)
             {
                 data += bonePos[j].ToString();
             }
@@ -64,9 +68,11 @@ public class GloveRecorder : MonoBehaviour
 
         data += "," + gesture;
         writer.WriteLine(data);
+        writer.Close();
 
         recording = false;
     }
+
 
     // Use this for initialization
     void Start()
@@ -76,19 +82,18 @@ public class GloveRecorder : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if( !recording)
+    {   
+        if (Input.GetKeyDown(KeyCode.R))        //R for record
         {
-            // JUST FOR TESTING; we'll add adjustments for gestures
-            Hand hand = Hand.RIGHT;
-            Gesture gesture = Gesture.A;
+            if (!recording)
+            {
+                // JUST FOR TESTING; we'll add adjustments for specifiations
+                Hand hand = Hand.RIGHT;
+                Gesture gesture = Gesture.None;
 
-            initializeWriter(hand, gesture);
+                initializeWriter(hand, gesture);
+            }
         }
-        //else
-        //{
-            //do stuff here (dispose the writer???)
-        //}
     }
 
 }
