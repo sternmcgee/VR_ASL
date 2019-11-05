7e1a64f85d6dbc56d18502560ea6d5a0c916d753
Jack Belcher <jbelcher_97@tamu.edu>
2019-11-05 10:00:25 -0600

    Fixing glove source

--- Assets/Scripts/Recorder/GloveRecorder.cs
+++ Assets/Scripts/Recorder/GloveRecorder.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
+using Unity;
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using HI5;
+
 
 public class GloveRecorder : MonoBehaviour
 {
     private enum Gesture { None, A, B, C, D, E, F, G, H, I, J, K, L,
                             M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z }
 
-    private HI5_Source handSource;
+    private HI5_Glove_TransformData_Interface Hands;
 
     private StreamWriter writer;
 
@@ -22,22 +25,22 @@ public class GloveRecorder : MonoBehaviour
     private void initializeWriter(Hand hand, Gesture gesture)
     {
         recording = true;
-        string path = dataPath + hand.ToString() + "_" + gesture.ToString() + "_" + DateTime.Now.ToString("MMddyyyyHHmmss") + ".csv";
-        writer = new StreamWriter(path);
+        writer = new StreamWriter(dataPath + hand.ToString() + "_" + gesture.ToString() +
+                                    "_" + DateTime.Now.ToString("MMddyyyyHHmmss") + ".csv");
         string header = "";
 
-        for (int i = 0; i < Enum.GetNames(typeof(Bones)).Length; ++i)
+        for (int i = 0; i < Enum.GetNames(typeof(Hands.EHi5_Glove_TransformData_Bones)).Length; ++i)
         {
-            string[] cord = { "x", "y", "z" };
+            string[] cord = { "x", "y", "z", "w" };
 
             for (int j = 0; i < 3; ++i)
             {
-                if (i == 0 && j == 0) { header += (Bones)i + "_pos" + cord[j]; }
-                else { header += "," + (Bones)i + "_pos" + cord[j]; }
+                if (i == 0 && j == 0) { header += (Hands.EHi5_Glove_TransformData_Bones)i + "_pos" + cord[j]; }
+                else { header += "," + (Hands.EHi5_Glove_TransformData_Bones)i + "_pos" + cord[j]; }
             }
             foreach (string s in cord)
             {
-                header += "," + "_quad" + (Bones)i + s;
+                header += "," + "_quad" + (Hands.EHi5_Glove_TransformData_Bones)i + s;
             }
         }
         header += ",gesture";
@@ -53,19 +56,26 @@ public class GloveRecorder : MonoBehaviour
         for (int x = 0; x < 100; ++x)
         {
             string data = "";
-            for (int i = 0; i < Enum.GetNames(typeof(Bones)).Length; ++i)
+            for (int i = 0; i < Enum.GetNames(typeof(Hands.EHi5_Glove_TransformData_Bones)).Length; ++i)
             {
-                Vector3 bonePos = handSource.GetReceivedPosition(i, hand);
-                Vector3 boneRot = handSource.GetReceivedRotation(i, hand);
+                //Vector3 bonePos = Hands.GetReceivedPosition(i, hand);
+                //Vector3 boneRot = Hands.GetReceivedRotation(i, hand);
 
                 for (int j = 0; j < 3; ++j)
                 {
-                    if (i == 0 && j == 0) { data += bonePos[j].ToString(); }
-                    else { data += "," + bonePos[j].ToString(); }
+                    //if (i == 0 && j == 0) { data += bonePos[j].ToString(); }
+                    //else { data += "," + bonePos[j].ToString(); }
+
+                    if (i == 0 && j == 0)
+                    {
+                        data += Hands.GetLeftHandTransform()[(Hands.EHi5_Glove_TransformData_Bones)i].localPosition[j];
+                    }
+                    else { data += "," + Hands.GetLeftHandTransform()[(Hands.EHi5_Glove_TransformData_Bones)i].localPosition[j]; }
                 }
-                for (int j = 0; j < 3; ++j)
+                for (int j = 0; j < 4; ++j)
                 {
-                    data += "," + boneRot[j].ToString();
+                    //data += "," + boneRot[j].ToString();
+                    data += "," + Hands.GetLeftHandTransform()[(Hands.EHi5_Glove_TransformData_Bones)i].localRotation[j];
                 }
             }
             data += "," + gesture;
