3573ec1f12e377753f03818e8b29b10b5ee807cc
darwi <darwi@LAPTOP-1BIGBJBB>
2019-11-07 10:08:36 -0600

    Adding new main menu scene

---
+++ Assets/EasyMainMenu/Scripts/Main Menu Scripts/MainMenu2.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu2 : MonoBehaviour
+{
+    public string newGameSceneName;
+    public int quickSaveSlotID;
+
+    [Header("Options Panel")]
+    public GameObject MainScreenPanel;
+    public GameObject StartGameOptionsPanel;
+    //public GameObject GamePanel;
+    //public GameObject LoadGamePanel;
+    public GameObject ChoosingGamePanel;
+    public GameObject LectureTransition;
+    public GameObject FallingLetters;
+    public GameObject ThrowingHands;
+
+    // Use this for initialization
+    void Start()
+    {
+        //anim = GetComponent<Animator>();
+        MainScreenPanel.SetActive(true);
+
+        //new key
+        PlayerPrefs.SetInt("quickSaveSlot", quickSaveSlotID);
+    }
+
+    #region Open Different panels
+
+    public void openStartGameOptions()
+    {
+        //enable respective panel
+        StartGameOptionsPanel.SetActive(true);
+        MainScreenPanel.SetActive(false);
+
+        //play anim for opening main options panel
+        //anim.Play("buttonTweenAnims_on");
+
+        //play click sfx
+        playClickSound();
+
+        Debug.Log("Start Game Go");
+
+        //enable BLUR
+        //Camera.main.GetComponent<Animator>().Play("BlurOn");
+
+    }
+
+    public void openStartGameOptions_Lecture()
+    {
+        //enable respective panel
+        //StartGameOptionsPanel.SetActive(true);
+        LectureTransition.SetActive(true);
+
+        //play anim for opening main options panel
+        //anim.Play("buttonTweenAnims_on");
+
+        //play click sfx
+        playClickSound();
+
+        //enable BLUR
+        //Camera.main.GetComponent<Animator>().Play("BlurOn");
+    }
+
+    public void openFallingLetters()
+    {
+        //enable respective panel
+        StartGameOptionsPanel.SetActive(false);
+        FallingLetters.SetActive(true);
+
+        //play anim for opening main options panel
+        //anim.Play("buttonTweenAnims_on");
+
+        //play click sfx
+        playClickSound();
+
+        //enable BLUR
+        //Camera.main.GetComponent<Animator>().Play("BlurOn");
+    }
+
+    public void openThrowingHands()
+    {
+        //enable respective panel
+        StartGameOptionsPanel.SetActive(false);
+        ThrowingHands.SetActive(true);
+
+        //play anim for opening main options panel
+        //anim.Play("buttonTweenAnims_on");
+
+        //play click sfx
+        playClickSound();
+
+        //enable BLUR
+        //Camera.main.GetComponent<Animator>().Play("BlurOn");
+    }
+
+    public void openLecture()
+    {
+        //enable respective panel
+        StartGameOptionsPanel.SetActive(true);
+        LectureTransition.SetActive(true);
+
+        //play anim for opening main options panel
+        //anim.Play("buttonTweenAnims_on");
+
+        //play click sfx
+        playClickSound();
+
+        //enable BLUR
+        //Camera.main.GetComponent<Animator>().Play("BlurOn");
+    }
+
+    public void openStartGameOptions_Games()
+    {
+        //enable respective panel
+        ChoosingGamePanel.SetActive(true);
+        StartGameOptionsPanel.SetActive(false);
+        MainScreenPanel.SetActive(false);
+
+        //play click sfx
+        playClickSound();
+
+    }
+
+    public void newGame()
+    {
+        
+    }
+    #endregion
+
+    #region Back Buttons
+
+    public void back_options()
+    {
+
+        //disable BLUR
+        // Camera.main.GetComponent<Animator>().Play("BlurOff");
+
+        //play click sfx
+        playClickSound();
+    }
+
+    public void back_options_panels()
+    {
+
+        //play click sfx
+        playClickSound();
+
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    #endregion
+
+    #region Sounds
+    public void playHoverClip()
+    {
+
+    }
+
+    void playClickSound()
+    {
+
+    }
+
+
+    #endregion
+
+}
