768737ee3b009af8812cc3d4eacd4abb80406076
darwi <darwi@LAPTOP-1BIGBJBB>
2019-11-18 20:53:39 -0600

    Add box collider and interactable to start button in mainmenu

---
+++ Assets/Imported Assets/EasyMainMenu/Scripts/Main Menu Scripts/MainMenuBack.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuBack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject MainMenuTransition;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void openMainMenu()
+    {
+        //enable respective panel
+        MainMenuTransition.SetActive(true);
+
+    }
+}
